ed6d2d06367a3ca3a55fd416a7302b8033a12631
mtkwebdev <mtkdev1.0@gmail.com>
2022-10-27 13:29:35 +0100

    Adding patient records and specimens

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-
-const tabSelector = document.querySelector(".tab-selector");
-
 // tab selector buttons 
-const newPatient = document.querySelector("#add-new-patient");
-const addSpecimen = document.querySelector("#add-specimen");
-const showSpecimens = document.querySelector("#show-specimens");
+const tabSelector = document.querySelector(".tab-selector");
+const newPatientTab = document.querySelector("#add-new-patient");
+const addSpecimenTab = document.querySelector("#add-specimen");
+const showSpecimensTab = document.querySelector("#show-specimens");
 
 // tabs to select
 const patientRecordContainer = document.querySelector("#patient-record-container");
 const patientSpecimensContainer = document.querySelector("#patient-specimens-container");
 const patientResultsContainer = document.querySelector("#patient-results-container");
 
-// Dry functions 
+//forms 
+const patientRecordContainerForm = document.getElementById("patient-record-container-form");
+const patientSpecimensContainerForm = document.getElementById("patient-specimens-container-form");
+// const patientRecordContainerForm = document.getElementById("patient-record-container-form");
+
+// specific inputs 
+const selectPatient = document.getElementById("select-patient")
+
+// data stores
+let allPatientRecords = []
+let allSpecimens = []
+let allPatientResults = []
+let newPatientRecord = {}
+let newPatientSpecimen = {}
+
+// generic reusable functions 
 	function removeAllTabButtonActiveClasses() {
 		const tabButton = document.querySelectorAll(".tab-button");
 		for (let i = 0; i < tabButton.length; i++) {
@@ -26,56 +39,116 @@ const patientResultsContainer = document.querySelector("#patient-results-contain
 		}
 	}
 
-	tabSelector.addEventListener("click", (e) => {
-		// console.log(e.target.id);
-		switch (e.target.id) {
-			case "add-new-patient":
-				removeAllTabButtonActiveClasses();
-				newPatient.classList.add("tab-active");
-				hideAllTabContent()
-				patientRecordContainer.classList.remove("hide");
+	function toggleHideAndShow(selectAll, remove, add){
+		const htmlSelector = document.querySelectorAll(selectAll);
+		if(remove){
+			for (let i = 0; i < htmlSelector.length; i++) {
+				htmlSelector[i].classList.remove(remove);
+			}
+		}else{
+			for (let i = 0; i < htmlSelector.length; i++) {
+				htmlSelector[i].classList.add(add);
+			}
+		}
+	}
+
+	function betterToggleHideAndShow(selectAll, {remove, add}){
+		const htmlSelector = document.querySelectorAll(selectAll);
+		if(remove){
+			for (let i = 0; i < htmlSelector.length; i++) {
+				htmlSelector[i].classList.remove(remove);
+			}
+		}else{
+			for (let i = 0; i < htmlSelector.length; i++) {
+				htmlSelector[i].classList.add(add);
+			}
+		}
+	}
+
+	const addPatientRecord= () => {
+		newPatientRecord = {}
+		newPatientRecord.lastName = document.querySelector('#patientLastName').value
+		newPatientRecord.firstNames = document.querySelector('#patientFirstNames').value
+		newPatientRecord.patientAddress = document.querySelector('#patientAddress').value
+		newPatientRecord.patientDOB = document.querySelector('#patientDOB').value
+		newPatientRecord.patientCRN = document.querySelector('#patientCRN').value
+
+		let duplicateRecord = allPatientRecords.findIndex(existingRecords => existingRecords.patientCRN === newPatientRecord.patientCRN)
+	
+		if(duplicateRecord === -1){
+			allPatientRecords.push(newPatientRecord)
+			const addNewOption = document.createElement('option')
+			addNewOption.innerText = `${newPatientRecord.patientCRN} / ${newPatientRecord.patientDOB} - ${newPatientRecord.lastName}`
+			selectPatient.appendChild(addNewOption)
+
+		}else{ return }
+	}
+
+	function addSpecimensToRecord(){
+		newPatientSpecimen = {}
+		newPatientSpecimen.patient = document.querySelector('#select-patient').value
+		newPatientSpecimen.specimen = document.querySelector('#select-specimen').value
+		newPatientSpecimen.triage = document.querySelector('#select-triage').value
+		newPatientSpecimen.description = document.querySelector('#sample-description').value
+
+		let duplicateSpecimen = allSpecimens.findIndex(existingSpecimens => String(Object.values(existingSpecimens)) === String(Object.values(newPatientSpecimen)))
+		console.log(String(Object.values(newPatientSpecimen)))
+
+		if(duplicateSpecimen === -1){
+			allSpecimens.push(newPatientSpecimen)
+		}
+		
+	}
+
+
+tabSelector.addEventListener("click", (e) => {
+	// console.log(e.target.id);
+	switch (e.target.id) {
+		case "add-new-patient":
+			// removeAllTabButtonActiveClasses();
+			// hideAllTabContent()
+			// toggleHideAndShow(".tab-button","tab-active")
+			// toggleHideAndShow(".tab-content", null, "hide")
+			betterToggleHideAndShow(".tab-button",{remove:"tab-active", add:null})
+			betterToggleHideAndShow(".tab-content", {remove:null, add:"hide"})
+			newPatientTab.classList.add("tab-active");
+			patientRecordContainer.classList.remove("hide");
+			break;
+			
+			case "add-specimen":
+			// removeAllTabButtonActiveClasses();
+			// hideAllTabContent()
+			// toggleHideAndShow(".tab-button","tab-active")
+			// toggleHideAndShow(".tab-content", null, "hide")
+			betterToggleHideAndShow(".tab-button",{remove:"tab-active", add:null})
+			betterToggleHideAndShow(".tab-content", {remove:null, add:"hide"})
+				addSpecimenTab.classList.add("tab-active");
+				patientSpecimensContainer.classList.remove("hide");
 				break;
 				
-				case "add-specimen":
-					removeAllTabButtonActiveClasses();
-					addSpecimen.classList.add("tab-active");
-					hideAllTabContent()
-					patientSpecimensContainer.classList.remove("hide");
+				case "show-specimens":
+			// removeAllTabButtonActiveClasses();
+			// hideAllTabContent()
+			// toggleHideAndShow(".tab-button","tab-active")
+			// toggleHideAndShow(".tab-content", null, "hide")
+			betterToggleHideAndShow(".tab-button",{remove:"tab-active", add:null})
+			betterToggleHideAndShow(".tab-content", {remove:null, add:"hide"})
+					showSpecimensTab.classList.add("tab-active");
+					patientResultsContainer.classList.remove("hide");
 					break;
-					
-					case "show-specimens":
-						removeAllTabButtonActiveClasses();
-						showSpecimens.classList.add("tab-active");
-						hideAllTabContent()
-						patientResultsContainer.classList.remove("hide");
-						break;
-		}
-	});
-	
-let patientRecord ={}
-let allPatientRecords =[]
-let patientSpecimen ={}
-let allSpecimens = []
-let allPatientResults =[]
-
-const patientRecordContainerForm = document.getElementById("patient-record-container-form");
-patientRecordContainerForm.addEventListener('submit', (e) => {
-	// stop form submission
-	e.preventDefault();
-		patientRecord.lastName = document.querySelector('#patientLastName').value
-		patientRecord.firstNames = document.querySelector('#patientFirstNames').value
-		patientRecord.patientAddress = document.querySelector('#patientAddress').value
-		patientRecord.patientDOB = document.querySelector('#patientDOB').value
-		patientRecord.patientCRN = document.querySelector('#patientCRN').value
+	}
 });
 
-// const patientRecordContainerForm = document.getElementById("patient-record-container-form");
-// patientRecordContainerForm.addEventListener('submit', (e) => {
-// 	// stop form submission
-// 	e.preventDefault();
-// 		patientRecord.lastName = document.querySelector('#patientLastName').value
-// 		patientRecord.firstNames = document.querySelector('#patientFirstNames').value
-// 		patientRecord.patientAddress = document.querySelector('#patientAddress').value
-// 		patientRecord.patientDOB = document.querySelector('#patientDOB').value
-// 		patientRecord.patientCRN = document.querySelector('#patientCRN').value
-// });
+
+patientRecordContainerForm.addEventListener('submit', (e) => {
+	// stop automatic page reload
+	e.preventDefault();
+	addPatientRecord()
+	console.log(allPatientRecords)
+});
+
+patientSpecimensContainerForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	addSpecimensToRecord()
+	console.log(allSpecimens)
+})
